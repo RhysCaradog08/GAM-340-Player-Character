b650b65d65b8c5127284d68008459b47abd2ca6a
rc216430 <rc216430@falmouth.ac.uk>
2021-02-07 17:22:39 +0000

    Iterating Destructable Objects
    Created Wooden Crate & Stone Block prefabs

--- New Unity Project/Assets/Scripts/DestructableObject.cs
+++ New Unity Project/Assets/Scripts/DestructableObject.cs
@@ -8,7 +8,6 @@ public class DestructableObject : MonoBehaviour
     [SerializeField] MeshRenderer brokenMesh;
 
     public float health;
-    public bool isStone;
 
     // Start is called before the first frame update
     void Start()
