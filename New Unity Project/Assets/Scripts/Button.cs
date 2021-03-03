e4238ec8efc36c650e9817950e549f04b579c4a6
rc216430 <rc216430@falmouth.ac.uk>
2021-03-03 20:16:47 +0000

    Creating Showcase Level

---
+++ New Unity Project/Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other .CompareTag("Throwable"))
+        {
+            Debug.Log("Button Pressed");
+
+            anim.SetTrigger("Activated");
+        }
+    }
+}
