113a381c083886a100e1e1afcd7a1d622f514d38
Reece Cradock <rc216430@falmouth.ac.uk>
2021-03-11 12:23:31 +0000

    UI triggers implemented

---
+++ New Unity Project/Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTrigger : MonoBehaviour
+{
+    public GameObject gameText;
+
+    public Text text;
+
+    [TextArea]
+    public string instructions;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            gameText.SetActive(true);
+
+            text.text = instructions;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        gameText.SetActive(false);
+    }
+}
